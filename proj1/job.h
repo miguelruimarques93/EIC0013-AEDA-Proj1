512d6a3bb2b3af1f8d12b3e64814a095ca5dd3a4
migulorama <migulorama@gmail.com>
2012-09-27 18:59:40 +0100

    Created Job and Machine classes.

---
+++ proj1/job.h
@@ -0,0 +1,14 @@
+#ifndef JOB_H_
+#define JOB_H_
+
+class Job
+{
+public:
+
+protected:
+
+private:
+
+};
+
+#endif // JOB_H_
---
+++ proj1/machine.h
@@ -0,0 +1,14 @@
+#ifndef MACHINE_H_
+#define MACHINE_H_
+
+class Machine
+{
+public:
+	
+protected:
+
+private:
+
+};
+
+#endif // MACHINE_H_
