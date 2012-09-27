fb8d88b673099d68ffa2f647395bed185428f4fa
migulorama <migulorama@gmail.com>
2012-09-27 18:57:18 +0100

    Created classes User, AcademicUser and EnterpriseUser.

---
+++ proj1/user.h
@@ -0,0 +1,34 @@
+#ifndef USER_H_
+#define USER_H_
+
+class User
+{
+public:
+
+protected:
+
+private:
+
+};
+
+class AcademicUser : public User
+{
+public:
+
+protected:
+
+private:
+	
+};
+
+class EnterpriseUser : public User
+{
+public:
+
+protected:
+
+private:
+
+};
+
+#endif // USER_H_
