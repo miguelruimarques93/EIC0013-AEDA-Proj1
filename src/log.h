fd2d26fac115f71eefca9dfdc5a07409eb3f199b
Nay <dnpd.dd@gmail.com>
2012-11-03 16:47:17 +0000

    Logger documentation

--- src/log.h
+++ src/log.h
@@ -8,16 +8,19 @@
 #include <cstdarg>
 #include <sstream>
 
+/// Base class for Writers
 struct Writer
 {
-    virtual void Write(const std::string& val) = 0;
+    virtual void Write(const std::string& val) = 0; ///> Writes a message to the logger
 };
 
+/// Writer for console
 struct ConsoleWritePolicy : public Writer
 {
     void Write(const std::string& val) { std::cout << '\r' << val << std::endl; }
 };
 
+/// Writer for file (log.txt)
 struct FileWritePolicy : public Writer
 {
     FileWritePolicy() { _file.open("log.txt"); }
@@ -29,10 +32,15 @@ struct FileWritePolicy : public Writer
     std::ofstream _file;
 };
 
+//! Logger singleton Class
+/*!
+    A class that enables to log messages to files, console and others
+*/
 template <class WritePolicy>
 class Logger
 {
 public:
+    /// Returns the singleton's instance of this class
     static Logger* Instance()
     {
         if (!_instance)
@@ -40,6 +48,7 @@ public:
         return _instance;
     }
 
+    /// Writes a message (printf style). All messages will be prefixed with current time.
     void Log(const char* format, ...)
     {
         char buffer[512];
@@ -62,16 +71,21 @@ public:
     }
 
 private:
-    static Logger* _instance;
-    std::mutex _mutex;
-    WritePolicy _writer;
+    static Logger* _instance; ///> There's only one instance of this class
+    std::mutex _mutex; ///> Mutex to make the class safe to multiple threads
+    WritePolicy _writer; ///> A writer (console, file, etc.)
 };
 
 template <class WritePolicy>
 Logger<WritePolicy>* Logger<WritePolicy>::_instance = NULL;
 
+/// Alias for ConsoleWritePolicy
 #define Console ConsoleWritePolicy
+
+/// Alias for FileWritePolicy
 #define FileWP FileWritePolicy
+
+/// Short way to access the log. Example: sLog(Console)->Log("Message here.");
 #define sLog(policy) Logger<policy>::Instance()
 
 #endif // LOG_H_
