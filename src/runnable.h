16e6cea32d06ee349927aec77771ff67312f6f3b
Nay <dnpd.dd@gmail.com>
2012-11-03 16:21:46 +0000

    Runnable documentation

--- src/runnable.h
+++ src/runnable.h
@@ -4,21 +4,25 @@
 #include <atomic>
 #include <thread>
 
+//! Runnable abstract Class
+/*!
+    Represents a class that can be used in a multithreaded way
+*/
 class Runnable
 {
 public:
-    Runnable() : _stop(), _thread() { }
-    virtual ~Runnable() { Stop(); }
+    Runnable() : _stop(), _thread() { } ///> Constructor.
+    virtual ~Runnable() { Stop(); } ///> Destructor. Stops thread.
 
-    void Stop() { _stop = true; _thread.join(); }
-    void Start() { _thread = std::thread(&Runnable::Run, this); }
+    void Stop() { _stop = true; _thread.join(); } ///> Stops thread.
+    void Start() { _thread = std::thread(&Runnable::Run, this); } ///> Starts thread.
 
 protected:
-    virtual void Run() = 0;
-    std::atomic<bool> _stop;
+    virtual void Run() = 0; ///> Method to execute when thread is started.
+    std::atomic<bool> _stop; ///> True if the thread should be stopped.
 
 private:
-    std::thread _thread;
+    std::thread _thread; ///> The thread.
 
 private: // no copying
     Runnable(Runnable const&);
