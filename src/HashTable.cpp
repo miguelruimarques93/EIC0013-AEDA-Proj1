bd756c250c09673f59c4ce726ecf87bfef07a1bd
Freixo <hugo1245@gmail.com>
2012-12-14 22:37:48 +0000

    Aqui esté DUdu

---
+++ src/HashTable.cpp
@@ -0,0 +1,44 @@
+#include "HashTable.h"
+
+bool HashTable::Empty()
+{
+	return _hashTable.empty();
+}
+
+
+void HashTable::InsertUser(User* user)
+{
+	_hashTable.insert(user);
+}
+
+bool HashTable::RemoveUser( const std::string& name )
+{
+	for (auto it = _hashTable.begin(); it != _hashTable.end(); ++it)
+	{
+		if ((*it)->GetName() == name)
+		{
+			_hashTable.erase(it);
+			return true;
+		}
+	}
+	return false;
+}
+
+bool HashTable::RemoveUser( const uint& id )
+{
+	for (auto it = _hashTable.begin(); it != _hashTable.end(); ++it)
+	{
+		if ((*it)->GetId() == id)
+		{
+			_hashTable.erase(it);
+			return true;
+		}
+	}
+	return false;
+}
+
+void HashTable::Update( uint32 diff )
+{
+	throw std::exception("The method or operation is not implemented.");
+}
+
